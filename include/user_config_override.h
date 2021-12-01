9535193d75b32609aac0e7ea9a0f8d7679f43c6f
Deepak Divakar <deepak112358@gmail.com>
2021-12-02 03:42:13 +0530

    Create user_config_override.h

---
+++ include/user_config_override.h
@@ -0,0 +1,132 @@
+/* MIT License - Copyright (c) 2019-2021 Francis Van Roie
+   For full license information read the LICENSE file in the project folder */
+
+/***************************************************
+// This file contains the default settings that are
+// burned into the compiled firmware.
+//
+// These default settings can be changed at runtime
+//
+// To use: Save a copy as user_config_override.h
+***************************************************/
+#ifndef HASP_USER_CONFIG_OVERRIDE_H
+#define HASP_USER_CONFIG_OVERRIDE_H
+
+#define SERIAL_SPEED 115200
+
+/***************************************************
+          WiFi Settings
+ **************************************************/
+#define WIFI_SSID "DV Home 2G"
+#define WIFI_PASSW "19901993"
+
+/***************************************************
+          MQTT Settings
+ **************************************************/
+#define MQTT_HOST "192.168.29.150"
+#define MQTT_PORT 1883
+#define MQTT_USER "deepak-divakar"
+#define MQTT_PASSW "huedye"
+#define MQTT_PREFIX "hasp"
+#define MQTT_NODENAME "dv_home"
+#define MQTT_GROUPNAME "plates"
+
+#define MQTT_TELEPERIOD 60000
+#define MQTT_STATEPERIOD 300000
+
+/***************************************************
+ *        Server Settings
+ **************************************************/
+#define OTA_HOSTNAME ""
+#define OTA_SERVER ""
+#define OTA_PORT 80
+#define OTA_URL ""
+#define OTA_PASSWORD ""
+
+/***************************************************
+ *        Syslog Settings
+ **************************************************/
+#define SYSLOG_SERVER "192.168.29.150"
+#define SYSLOG_PORT 514
+#define APP_NAME "HASP"
+
+/***************************************************
+ *        Timezone Settings
+ **************************************************/
+#define MYTZ                                                                                                           \
+    "IST-5:30" // A full list with possible timezones can be found here
+                                 // https://gist.github.com/alwynallan/24d96091655391107939
+
+/***************************************************
+ *        Interface Language Settings
+ **************************************************/
+#define HASP_LANGUAGE en_US // English
+// #define HASP_LANGUAGE es_ES                       // Spanish
+// #define HASP_LANGUAGE fr_FR                       // French
+// #define HASP_LANGUAGE hu_HU                       // Hungarian
+// #define HASP_LANGUAGE nl_NL                       // Dutch
+// #define HASP_LANGUAGE pt_BR                       // Brazilian Portuguese
+// #define HASP_LANGUAGE pt_PT                       // Portuguese
+// #define HASP_LANGUAGE ro_RO                       // Romanian
+
+/***************************************************
+ *        Web interface coloring
+ **************************************************/
+// Light theme (default)
+#define D_HTTP_COLOR_TEXT "#000"          // Global text color - Black
+#define D_HTTP_COLOR_BACKGROUND "#fff"    // Global background color - White
+#define D_HTTP_COLOR_INPUT_TEXT "#000"    // Input text color - Black
+#define D_HTTP_COLOR_INPUT "#fff"         // Input background color - White
+#define D_HTTP_COLOR_INPUT_WARNING "#f00" // Input warning border color - Red
+#define D_HTTP_COLOR_BUTTON_TEXT "#fff"   // Button text color - White
+#define D_HTTP_COLOR_BUTTON "#1fa3ec"     // Button color - Vivid blue
+#define D_HTTP_COLOR_BUTTON_RESET "#f00"  // Restart/Reset button color - red
+
+/*
+// Dark theme
+#define D_HTTP_COLOR_TEXT               "#eaeaea"    // Global text color - Very light gray
+#define D_HTTP_COLOR_BACKGROUND         "#252525"    // Global background color - Very dark gray (mostly black)
+#define D_HTTP_COLOR_INPUT_TEXT         "#000"       // Input text color - Black
+#define D_HTTP_COLOR_INPUT              "#ddd"       // Input background color - Very light gray
+#define D_HTTP_COLOR_INPUT_WARNING      "#ff5661"    // Warning text color - Brick Red
+#define D_HTTP_COLOR_BUTTON_TEXT        "#faffff"    // Button text color - Very pale (mostly white) cyan
+#define D_HTTP_COLOR_BUTTON             "#1fa3ec"    // Button color - Vivid blue
+#define D_HTTP_COLOR_BUTTON_RESET       "#d43535"    // Restart/Reset/Delete button color - Strong red
+*/
+
+/***************************************************
+ *        Font Settings
+ **************************************************/
+#define HASP_FONT_1 robotocondensed_regular_20_latin1 // Use available fonts from src/fonts directory
+#define HASP_FONT_2 robotocondensed_regular_24_latin1
+#define HASP_FONT_3 robotocondensed_regular_32_latin1
+#define HASP_FONT_4 robotocondensed_regular_48_latin1
+
+#define ROBOTOCONDENSED_REGULAR_20_LATIN1 1           // Enable the selected fonts
+#define ROBOTOCONDENSED_REGULAR_24_LATIN1 1
+#define ROBOTOCONDENSED_REGULAR_32_LATIN1 1
+#define ROBOTOCONDENSED_REGULAR_48_LATIN1 1
+
+#define HASP_FONT_SIZE_1 20                           // Define used font sizes
+#define HASP_FONT_SIZE_2 24
+#define HASP_FONT_SIZE_3 32
+#define HASP_FONT_SIZE_4 48
+
+
+
+/***************************************************
+ *        GPIO Settings
+ **************************************************/
+//#define HASP_GPIO_TEMPLATE "[197658,263456,329249,655628,655886,656155,0,0]"  // Lanbon L8 3-gang GPIO config
+//#define HASP_GPIO_TEMPLATE "[3214348,197658,263456,329249,94699520,0,0,0]" // Lanbon L8 Dimmer GPIO config
+
+/***************************************************
+ *        Other Settings
+ **************************************************/
+//#define HASP_USE_CUSTOM 1                           // Enable compilation of custom code from /src/custom
+//#define HASP_USE_HA                                 // Enable Home Assistant auto-discovery
+//#define HASP_START_CONSOLE 0                        // Disable starting of serial console at boot
+//#define HASP_START_TELNET 0                         // Disable starting of telnet service at boot
+//#define HASP_START_HTTP 0                           // Disable starting of web interface at boot
+
+#endif
